4d0f1b0768bf5d639c9e2cd89a94cb9c6ac81059
Sylvain Maillard <maillard.sy@gmail.com>
2024-10-04 17:55:34 +0200

    Add styles project + Test project

---
+++ src/OpenSilver.Themes.Modern/ModernThemeTest/ModernThemeTest/MainPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using OpenSilver.Themes.Modern;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernThemeTest
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            ThemeSelector.SelectTheme(ThemesSelection.Bright);
+            this.InitializeComponent();
+
+            this.DataContext = new DtCtx();
+            // Enter construction logic here...
+        }
+    }
+
+    public class DtCtx : INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+        public List<Person> People = Person.GetPeople();
+        string _someString;
+        [Required(ErrorMessage = "SomeString is required")]
+        public string SomeString
+        {
+            get { return _someString; }
+            set { _someString = value; OnPropertyChanged(); UpdateValidation(); }
+        }
+
+        private void UpdateValidation()
+        {
+            ValidateProperty("SomeString", _someString);
+        }
+
+        private Dictionary<string, List<string>> _errors = new Dictionary<string, List<string>>();
+
+        public bool HasErrors => _errors.Count > 0;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (_errors.ContainsKey(propertyName))
+            {
+                return _errors[propertyName];
+            }
+            return null;
+        }
+
+        protected void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        private void ValidateProperty(string propertyName, object value)
+        {
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(this) { MemberName = propertyName };
+
+            if (!Validator.TryValidateProperty(value, context, results))
+            {
+                _errors[propertyName] = new List<string>();
+                foreach (var validationResult in results)
+                {
+                    _errors[propertyName].Add(validationResult.ErrorMessage);
+                }
+                OnErrorsChanged(propertyName);
+            }
+            else if (_errors.ContainsKey(propertyName))
+            {
+                _errors.Remove(propertyName);
+                OnErrorsChanged(propertyName);
+            }
+        }
+    }
+
+    public class Person
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [DisplayName("First name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last name")]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        [DisplayName("Is VIP?")]
+        public bool IsVIP { get; set; }
+
+        public Person(string firstName, string lastName, int age, bool isVIP)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            IsVIP = isVIP;
+        }
+
+        public static List<Person> GetPeople()
+        {
+            var people = new List<Person>()
+            {
+                new Person("John", "Doe", 32, true),
+                new Person("Jessica", "Buck", 26, true),
+                new Person("Bob", "Deer", 56, false),
+                new Person("Georges", "Stag", 32, false),
+                new Person("Elma", "Elk", 32, true)
+            };
+            return people;
+        }
+    }
+}
---
+++ src/OpenSilver.Themes.Modern/OpenSilver.Themes.Modern/ThemeSelector.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OpenSilver.Themes.Modern
+{
+    static public class ThemeSelector
+    {
+        static string _assemblyName;
+        static ThemesSelection _currentTheme;
+
+        public static StyleType StyleType { get; set; } = StyleType.Implicit;
+
+        static ThemeSelector()
+        {
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            _assemblyName = currentAssembly.GetName().Name;
+        }
+
+        public static void ApplyApplicationStyle()
+        {
+            LoadResourceFile("StylesDesign.xaml");
+        }
+
+        static public void SelectTheme(ThemesSelection theme)
+        {
+            _currentTheme = theme;
+            LoadResourceFile($"Themes/{theme.ToString()}Theme.xaml");
+            ApplyApplicationStyle();
+            SaveThemeToIsolatedStorage(theme);
+        }
+
+
+        static void LoadResourceFile(string path)
+        {
+            ResourceDictionary resource = new ResourceDictionary
+            {
+                Source = new Uri($"/{_assemblyName};component/{path}", UriKind.Relative)
+            };
+            Application.LoadComponent(resource, resource.Source);
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+
+        public static void SaveThemeToIsolatedStorage(ThemesSelection value)
+        {
+            IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
+            settings[nameof(ThemesSelection)] = value.ToString();
+        }
+
+        //public static void InitializeAI(ThemesSelection themeSelector, StyleType styleType = StyleType.Implicit)
+        //{
+        //    StyleType = styleType;
+        //    SelectTheme(themeSelector);
+        //}
+
+        //public static void InitializeAI()
+        //{
+        //    var themeSelection = RetrieveThemeFromIsolatedStorage();
+        //    SelectTheme(themeSelection);
+        //}
+
+        public static ThemesSelection GetCurrentTheme()
+        {
+            if (_currentTheme == ThemesSelection.None)
+                _currentTheme = RetrieveThemeFromIsolatedStorage();
+
+            return _currentTheme;
+        }
+
+        public static ThemesSelection RetrieveThemeFromIsolatedStorage()
+        {
+            IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
+
+            if (settings.TryGetValue(nameof(ThemesSelection), out string storedValue))
+            {
+                return (ThemesSelection)Enum.Parse(typeof(ThemesSelection), storedValue);
+            }
+            else
+            {
+                return ThemesSelection.Bright;
+            }
+        }
+    }
+
+    public enum ThemesSelection
+    {
+        None,
+        Dark,
+        Bright
+    }
+
+    public enum StyleType
+    {
+        Implicit,
+        None
+    }
+}
