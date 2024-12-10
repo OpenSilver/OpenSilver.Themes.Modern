42d1bf7b5444f3df11e627dc7444ccedd756497b
Andrei Arekhva <a.arekhva@gmail.com>
2024-12-10 13:06:13 +0300

    Remove "Theme_" prefix from names of Palette properties

--- src/OpenSilver.Themes.Modern/OpenSilver.Themes.Modern/Palettes/DarkPalette.cs
+++ src/OpenSilver.Themes.Modern/OpenSilver.Themes.Modern/Palettes/DarkPalette.cs
@@ -6,48 +6,48 @@ namespace OpenSilver.Themes.Modern;
 
 public class DarkPalette : Palette
 {
-    public override Color Theme_BackgroundColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#000000");
-    public override Color Theme_ContainerBackgroundColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#0D0D0D");
+    public override Color BackgroundColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#000000");
+    public override Color ContainerBackgroundColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#0D0D0D");
 
-    public override Color Theme_PrimaryColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#0C68E9");
-    public override Color Theme_ControlBackgroundColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#141414");
-    public override Color Theme_BorderColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#303030");
-    public override Color Theme_AlternateRowColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#212121");
-    public override Color Theme_TextColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#DEDEDE");
-    public override Color Theme_TextOnPrimaryColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FFFFFF");
-    public override Color Theme_DisabledColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#171717");
-    public override Color Theme_WatermarkColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#575757");
+    public override Color PrimaryColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#0C68E9");
+    public override Color ControlBackgroundColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#141414");
+    public override Color BorderColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#303030");
+    public override Color AlternateRowColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#212121");
+    public override Color TextColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#DEDEDE");
+    public override Color TextOnPrimaryColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FFFFFF");
+    public override Color DisabledColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#171717");
+    public override Color WatermarkColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#575757");
 
-    public override IValueConverter Theme_BrightnessColorConverter { get; set; } = new BrightenColorConverter();
+    public override IValueConverter BrightnessColorConverter { get; set; } = new BrightenColorConverter();
 
-    public override double Theme_HoverBrightnessRatio { get; set; } = 20;
-    public override double Theme_PressBrightnessRatio { get; set; } = 40;
+    public override double HoverBrightnessRatio { get; set; } = 20;
+    public override double PressBrightnessRatio { get; set; } = 40;
 
-    public override double Theme_ChartHaloOpacity { get; set; } = 1;
+    public override double ChartHaloOpacity { get; set; } = 1;
 
     //Colors for Charts
-    public override BrushEffectMode Theme_ChartsBrushEffectMode { get; set; } = BrushEffectMode.Linear;
-    public override Color Theme_ChartGridLineColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF1B1B1B");
-    public override Color Theme_ChartLegendForeground { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FFE5E5E5");
-    public override Color Theme_ChartAxisTextForeground { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF777777");
+    public override BrushEffectMode ChartsBrushEffectMode { get; set; } = BrushEffectMode.Linear;
+    public override Color ChartGridLineColor { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF1B1B1B");
+    public override Color ChartLegendForeground { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FFE5E5E5");
+    public override Color ChartAxisTextForeground { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF777777");
 
-    public override Color Theme_ChartSeriesColor1 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#BB33FF");
-    public override Color Theme_ChartSeriesColor2 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#F5FF33");
-    public override Color Theme_ChartSeriesColor3 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF6633");
-    public override Color Theme_ChartSeriesColor4 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#33FF66");
-    public override Color Theme_ChartSeriesColor5 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF1A75");
-    public override Color Theme_ChartSeriesColor6 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#33FFE0");
-    public override Color Theme_ChartSeriesColor7 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FFFFFF");
-    public override Color Theme_ChartSeriesColor8 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF4747");
-    public override Color Theme_ChartSeriesColor9 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#3399FF");
-    public override Color Theme_ChartSeriesColor10 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF66FF");
-    public override Color Theme_ChartSeriesColor11 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FFB833");
-    public override Color Theme_ChartSeriesColor12 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#8033FF");
-    public override Color Theme_ChartSeriesColor13 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#888888");
-    public override Color Theme_ChartSeriesColor14 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#CCCCCC");
-    public override Color Theme_ChartSeriesColor15 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#57FF33");
+    public override Color ChartSeriesColor1 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#BB33FF");
+    public override Color ChartSeriesColor2 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#F5FF33");
+    public override Color ChartSeriesColor3 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF6633");
+    public override Color ChartSeriesColor4 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#33FF66");
+    public override Color ChartSeriesColor5 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF1A75");
+    public override Color ChartSeriesColor6 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#33FFE0");
+    public override Color ChartSeriesColor7 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FFFFFF");
+    public override Color ChartSeriesColor8 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF4747");
+    public override Color ChartSeriesColor9 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#3399FF");
+    public override Color ChartSeriesColor10 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FF66FF");
+    public override Color ChartSeriesColor11 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#FFB833");
+    public override Color ChartSeriesColor12 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#8033FF");
+    public override Color ChartSeriesColor13 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#888888");
+    public override Color ChartSeriesColor14 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#CCCCCC");
+    public override Color ChartSeriesColor15 { get; set; } = RuntimeHelpers.ConvertFromInvariantString<Color>("#57FF33");
 
-    public override IValueConverter Theme_ChartSeriesColorConverter { get; set; } = new ColorToGradientStopsConverter();
+    public override IValueConverter ChartSeriesColorConverter { get; set; } = new ColorToGradientStopsConverter();
 
     
 
--- src/OpenSilver.Themes.Modern/OpenSilver.Themes.Modern/Palettes/Palette.cs
+++ src/OpenSilver.Themes.Modern/OpenSilver.Themes.Modern/Palettes/Palette.cs
@@ -7,6 +7,8 @@ namespace OpenSilver.Themes.Modern;
 
 public abstract class Palette
 {
+    private const string ThemePrefix = "Theme_";
+
     public static ResourceDictionary LoadPalette(Palette palette)
     {
         if (palette is null)
@@ -22,114 +24,114 @@ public abstract class Palette
 
     internal static void LoadPalette(Palette palette, ResourceDictionary resources)
     {
-        resources[nameof(Theme_BackgroundColor)] = palette.Theme_BackgroundColor;
-        resources["Theme_BackgroundBrush"] = new SolidColorBrush(palette.Theme_BackgroundColor);
+        resources[ThemePrefix + nameof(BackgroundColor)] = palette.BackgroundColor;
+        resources["Theme_BackgroundBrush"] = new SolidColorBrush(palette.BackgroundColor);
 
-        resources[nameof(Theme_ContainerBackgroundColor)] = palette.Theme_ContainerBackgroundColor;
-        resources["Theme_ContainerBackgroundBrush"] = new SolidColorBrush(palette.Theme_ContainerBackgroundColor);
+        resources[ThemePrefix + nameof(ContainerBackgroundColor)] = palette.ContainerBackgroundColor;
+        resources["Theme_ContainerBackgroundBrush"] = new SolidColorBrush(palette.ContainerBackgroundColor);
 
-        resources[nameof(Theme_PrimaryColor)] = palette.Theme_PrimaryColor;
-        resources["Theme_PrimaryBrush"] = new SolidColorBrush(palette.Theme_PrimaryColor);
+        resources[ThemePrefix + nameof(PrimaryColor)] = palette.PrimaryColor;
+        resources["Theme_PrimaryBrush"] = new SolidColorBrush(palette.PrimaryColor);
 
-        resources[nameof(Theme_ControlBackgroundColor)] = palette.Theme_ControlBackgroundColor;
-        resources["Theme_ControlBackgroundBrush"] = new SolidColorBrush(palette.Theme_ControlBackgroundColor);
+        resources[ThemePrefix + nameof(ControlBackgroundColor)] = palette.ControlBackgroundColor;
+        resources["Theme_ControlBackgroundBrush"] = new SolidColorBrush(palette.ControlBackgroundColor);
 
-        resources[nameof(Theme_BorderColor)] = palette.Theme_BorderColor;
-        resources["Theme_BorderBrush"] = new SolidColorBrush(palette.Theme_BorderColor);
+        resources[ThemePrefix + nameof(BorderColor)] = palette.BorderColor;
+        resources["Theme_BorderBrush"] = new SolidColorBrush(palette.BorderColor);
 
-        resources[nameof(Theme_AlternateRowColor)] = palette.Theme_AlternateRowColor;
-        resources["Theme_AlternateRowBrush"] = new SolidColorBrush(palette.Theme_AlternateRowColor);
+        resources[ThemePrefix + nameof(AlternateRowColor)] = palette.AlternateRowColor;
+        resources["Theme_AlternateRowBrush"] = new SolidColorBrush(palette.AlternateRowColor);
 
-        resources[nameof(Theme_TextColor)] = palette.Theme_TextColor;
-        resources["Theme_TextBrush"] = new SolidColorBrush(palette.Theme_TextColor);
+        resources[ThemePrefix + nameof(TextColor)] = palette.TextColor;
+        resources["Theme_TextBrush"] = new SolidColorBrush(palette.TextColor);
 
-        resources[nameof(Theme_TextOnPrimaryColor)] = palette.Theme_TextOnPrimaryColor;
-        resources["Theme_TextOnPrimaryBrush"] = new SolidColorBrush(palette.Theme_TextOnPrimaryColor);
+        resources[ThemePrefix + nameof(TextOnPrimaryColor)] = palette.TextOnPrimaryColor;
+        resources["Theme_TextOnPrimaryBrush"] = new SolidColorBrush(palette.TextOnPrimaryColor);
 
-        resources[nameof(Theme_DisabledColor)] = palette.Theme_DisabledColor;
-        resources["Theme_DisabledBrush"] = new SolidColorBrush(palette.Theme_DisabledColor);
+        resources[ThemePrefix + nameof(DisabledColor)] = palette.DisabledColor;
+        resources["Theme_DisabledBrush"] = new SolidColorBrush(palette.DisabledColor);
 
-        resources[nameof(Theme_WatermarkColor)] = palette.Theme_WatermarkColor;
-        resources["Theme_WatermarkBrush"] = new SolidColorBrush(palette.Theme_WatermarkColor);
+        resources[ThemePrefix + nameof(WatermarkColor)] = palette.WatermarkColor;
+        resources["Theme_WatermarkBrush"] = new SolidColorBrush(palette.WatermarkColor);
 
-        resources[nameof(Theme_BrightnessColorConverter)] = palette.Theme_BrightnessColorConverter;
+        resources[ThemePrefix + nameof(BrightnessColorConverter)] = palette.BrightnessColorConverter;
 
-        resources[nameof(Theme_HoverBrightnessRatio)] = palette.Theme_HoverBrightnessRatio;
-        resources[nameof(Theme_PressBrightnessRatio)] = palette.Theme_PressBrightnessRatio;
+        resources[ThemePrefix + nameof(HoverBrightnessRatio)] = palette.HoverBrightnessRatio;
+        resources[ThemePrefix + nameof(PressBrightnessRatio)] = palette.PressBrightnessRatio;
 
-        resources[nameof(Theme_ChartHaloOpacity)] = palette.Theme_ChartHaloOpacity;
+        resources[ThemePrefix + nameof(ChartHaloOpacity)] = palette.ChartHaloOpacity;
 
         //Colors for charts:
-        
-        resources[nameof(Theme_ChartsBrushEffectMode)] = palette.Theme_ChartsBrushEffectMode;
-        resources[nameof(Theme_ChartGridLineColor)] = palette.Theme_ChartGridLineColor;
-        resources["Theme_ChartGridLineBrush"] = new SolidColorBrush(palette.Theme_ChartGridLineColor);
-        resources[nameof(Theme_ChartLegendForeground)] = palette.Theme_ChartLegendForeground;
-        resources["Theme_ChartLegendForegroundBrush"] = new SolidColorBrush(palette.Theme_ChartLegendForeground);
-        resources[nameof(Theme_ChartAxisTextForeground)] = palette.Theme_ChartAxisTextForeground;
-        resources["Theme_ChartAxisTextForegroundBrush"] = new SolidColorBrush(palette.Theme_ChartAxisTextForeground);
-        resources[nameof(Theme_ChartSeriesColor1)] = palette.Theme_ChartSeriesColor1;
-        resources[nameof(Theme_ChartSeriesColor2)] = palette.Theme_ChartSeriesColor2;
-        resources[nameof(Theme_ChartSeriesColor3)] = palette.Theme_ChartSeriesColor3;
-        resources[nameof(Theme_ChartSeriesColor4)] = palette.Theme_ChartSeriesColor4;
-        resources[nameof(Theme_ChartSeriesColor5)] = palette.Theme_ChartSeriesColor5;
-        resources[nameof(Theme_ChartSeriesColor6)] = palette.Theme_ChartSeriesColor6;
-        resources[nameof(Theme_ChartSeriesColor7)] = palette.Theme_ChartSeriesColor7;
-        resources[nameof(Theme_ChartSeriesColor8)] = palette.Theme_ChartSeriesColor8;
-        resources[nameof(Theme_ChartSeriesColor9)] = palette.Theme_ChartSeriesColor9;
-        resources[nameof(Theme_ChartSeriesColor10)] = palette.Theme_ChartSeriesColor10;
-        resources[nameof(Theme_ChartSeriesColor11)] = palette.Theme_ChartSeriesColor11;
-        resources[nameof(Theme_ChartSeriesColor12)] = palette.Theme_ChartSeriesColor12;
-        resources[nameof(Theme_ChartSeriesColor13)] = palette.Theme_ChartSeriesColor13;
-        resources[nameof(Theme_ChartSeriesColor14)] = palette.Theme_ChartSeriesColor14;
-        resources[nameof(Theme_ChartSeriesColor15)] = palette.Theme_ChartSeriesColor15;
 
-        resources[nameof(Theme_ChartSeriesColorConverter)] = palette.Theme_ChartSeriesColorConverter;
+        resources[ThemePrefix + nameof(ChartsBrushEffectMode)] = palette.ChartsBrushEffectMode;
+        resources[ThemePrefix + nameof(ChartGridLineColor)] = palette.ChartGridLineColor;
+        resources["Theme_ChartGridLineBrush"] = new SolidColorBrush(palette.ChartGridLineColor);
+        resources[ThemePrefix + nameof(ChartLegendForeground)] = palette.ChartLegendForeground;
+        resources["Theme_ChartLegendForegroundBrush"] = new SolidColorBrush(palette.ChartLegendForeground);
+        resources[ThemePrefix + nameof(ChartAxisTextForeground)] = palette.ChartAxisTextForeground;
+        resources["Theme_ChartAxisTextForegroundBrush"] = new SolidColorBrush(palette.ChartAxisTextForeground);
+        resources[ThemePrefix + nameof(ChartSeriesColor1)] = palette.ChartSeriesColor1;
+        resources[ThemePrefix + nameof(ChartSeriesColor2)] = palette.ChartSeriesColor2;
+        resources[ThemePrefix + nameof(ChartSeriesColor3)] = palette.ChartSeriesColor3;
+        resources[ThemePrefix + nameof(ChartSeriesColor4)] = palette.ChartSeriesColor4;
+        resources[ThemePrefix + nameof(ChartSeriesColor5)] = palette.ChartSeriesColor5;
+        resources[ThemePrefix + nameof(ChartSeriesColor6)] = palette.ChartSeriesColor6;
+        resources[ThemePrefix + nameof(ChartSeriesColor7)] = palette.ChartSeriesColor7;
+        resources[ThemePrefix + nameof(ChartSeriesColor8)] = palette.ChartSeriesColor8;
+        resources[ThemePrefix + nameof(ChartSeriesColor9)] = palette.ChartSeriesColor9;
+        resources[ThemePrefix + nameof(ChartSeriesColor10)] = palette.ChartSeriesColor10;
+        resources[ThemePrefix + nameof(ChartSeriesColor11)] = palette.ChartSeriesColor11;
+        resources[ThemePrefix + nameof(ChartSeriesColor12)] = palette.ChartSeriesColor12;
+        resources[ThemePrefix + nameof(ChartSeriesColor13)] = palette.ChartSeriesColor13;
+        resources[ThemePrefix + nameof(ChartSeriesColor14)] = palette.ChartSeriesColor14;
+        resources[ThemePrefix + nameof(ChartSeriesColor15)] = palette.ChartSeriesColor15;
+
+        resources[ThemePrefix + nameof(ChartSeriesColorConverter)] = palette.ChartSeriesColorConverter;
     }
 
     internal static Palette Light { get; } = new LightPalette();
 
     internal static Palette Dark { get; } = new DarkPalette();
 
-    public abstract Color Theme_BackgroundColor { get; set; }
-    public abstract Color Theme_ContainerBackgroundColor { get; set; }
+    public abstract Color BackgroundColor { get; set; }
+    public abstract Color ContainerBackgroundColor { get; set; }
 
-    public abstract Color Theme_PrimaryColor { get; set; }
-    public abstract Color Theme_ControlBackgroundColor { get; set; }
-    public abstract Color Theme_BorderColor { get; set; }
-    public abstract Color Theme_AlternateRowColor { get; set; }
-    public abstract Color Theme_TextColor { get; set; }
-    public abstract Color Theme_TextOnPrimaryColor { get; set; }
-    public abstract Color Theme_DisabledColor { get; set; }
-    public abstract Color Theme_WatermarkColor { get; set; }
+    public abstract Color PrimaryColor { get; set; }
+    public abstract Color ControlBackgroundColor { get; set; }
+    public abstract Color BorderColor { get; set; }
+    public abstract Color AlternateRowColor { get; set; }
+    public abstract Color TextColor { get; set; }
+    public abstract Color TextOnPrimaryColor { get; set; }
+    public abstract Color DisabledColor { get; set; }
+    public abstract Color WatermarkColor { get; set; }
 
-    public abstract IValueConverter Theme_BrightnessColorConverter { get; set; }
+    public abstract IValueConverter BrightnessColorConverter { get; set; }
 
-    public abstract double Theme_HoverBrightnessRatio { get; set; }
-    public abstract double Theme_PressBrightnessRatio { get; set; }
+    public abstract double HoverBrightnessRatio { get; set; }
+    public abstract double PressBrightnessRatio { get; set; }
 
-    public abstract double Theme_ChartHaloOpacity { get; set; }
+    public abstract double ChartHaloOpacity { get; set; }
     //Colors for Charts
-    
-    public abstract BrushEffectMode Theme_ChartsBrushEffectMode { get; set; }
-    public abstract Color Theme_ChartGridLineColor { get; set; }
-    public abstract Color Theme_ChartLegendForeground { get; set; }
-    public abstract Color Theme_ChartAxisTextForeground { get; set; }
-    public abstract Color Theme_ChartSeriesColor1 { get; set; }
-    public abstract Color Theme_ChartSeriesColor2 { get; set; }
-    public abstract Color Theme_ChartSeriesColor3 { get; set; }
-    public abstract Color Theme_ChartSeriesColor4 { get; set; }
-    public abstract Color Theme_ChartSeriesColor5 { get; set; }
-    public abstract Color Theme_ChartSeriesColor6 { get; set; }
-    public abstract Color Theme_ChartSeriesColor7 { get; set; }
-    public abstract Color Theme_ChartSeriesColor8 { get; set; }
-    public abstract Color Theme_ChartSeriesColor9 { get; set; }
-    public abstract Color Theme_ChartSeriesColor10 { get; set; }
-    public abstract Color Theme_ChartSeriesColor11 { get; set; }
-    public abstract Color Theme_ChartSeriesColor12 { get; set; }
-    public abstract Color Theme_ChartSeriesColor13 { get; set; }
-    public abstract Color Theme_ChartSeriesColor14 { get; set; }
-    public abstract Color Theme_ChartSeriesColor15 { get; set; }
 
-    public abstract IValueConverter Theme_ChartSeriesColorConverter { get; set; }
+    public abstract BrushEffectMode ChartsBrushEffectMode { get; set; }
+    public abstract Color ChartGridLineColor { get; set; }
+    public abstract Color ChartLegendForeground { get; set; }
+    public abstract Color ChartAxisTextForeground { get; set; }
+    public abstract Color ChartSeriesColor1 { get; set; }
+    public abstract Color ChartSeriesColor2 { get; set; }
+    public abstract Color ChartSeriesColor3 { get; set; }
+    public abstract Color ChartSeriesColor4 { get; set; }
+    public abstract Color ChartSeriesColor5 { get; set; }
+    public abstract Color ChartSeriesColor6 { get; set; }
+    public abstract Color ChartSeriesColor7 { get; set; }
+    public abstract Color ChartSeriesColor8 { get; set; }
+    public abstract Color ChartSeriesColor9 { get; set; }
+    public abstract Color ChartSeriesColor10 { get; set; }
+    public abstract Color ChartSeriesColor11 { get; set; }
+    public abstract Color ChartSeriesColor12 { get; set; }
+    public abstract Color ChartSeriesColor13 { get; set; }
+    public abstract Color ChartSeriesColor14 { get; set; }
+    public abstract Color ChartSeriesColor15 { get; set; }
+
+    public abstract IValueConverter ChartSeriesColorConverter { get; set; }
 }
