220b633ce7e62d191379984ed733fd3888f71e2a
Pierre Barjon <barjon.pierre@gmail.com>
2024-12-30 22:58:52 +0100

    Improve handling of ComboBox's label

--- src/OpenSilver.Themes.Modern/OpenSilver.Themes.Modern/Helpers/FlatDesignProperties.cs
+++ src/OpenSilver.Themes.Modern/OpenSilver.Themes.Modern/Helpers/FlatDesignProperties.cs
@@ -1,43 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
-using System.Windows.Media;
 
 namespace OpenSilver.Themes.Modern
 {
     public static class FlatDesignProperties
     {
-        public static Brush GetForeground(DependencyObject obj)
-        {
-            return (Brush)obj.GetValue(ForegroundProperty);
-        }
-
-        public static void SetForeground(DependencyObject obj, Brush value)
-        {
-            obj.SetValue(ForegroundProperty, value);
-        }
-
-        // Using a DependencyProperty as the backing store for Foreground.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ForegroundProperty =
-            DependencyProperty.RegisterAttached("Foreground", typeof(Brush), typeof(FlatDesignProperties), new PropertyMetadata(new SolidColorBrush(Colors.Black)));
-
-
         public static string GetLabel(DependencyObject obj)
         {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
             return (string)obj.GetValue(LabelProperty);
         }
 
         public static void SetLabel(DependencyObject obj, string value)
         {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
             obj.SetValue(LabelProperty, value);
         }
 
-        // Using a DependencyProperty as the backing store for Label.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty LabelProperty =
-            DependencyProperty.RegisterAttached("Label", typeof(string), typeof(FlatDesignProperties), new PropertyMetadata(null));
-
-
-
+            DependencyProperty.RegisterAttached(
+                "Label",
+                typeof(string),
+                typeof(FlatDesignProperties),
+                new PropertyMetadata(string.Empty));
     }
 }
